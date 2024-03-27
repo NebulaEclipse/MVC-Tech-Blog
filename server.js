03f8258ea6764718016806d2cef8929a86ec08ed
NebulaEclipse <chrisdouglas200516@gmail.com>
2024-03-27 19:12:49 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,40 +1,43 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const allRoutes = require("./controllers");
-const session = require("express-session");
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./util/helper');
+const routes = require('./controllers');
 
 const app = express();
-const PORT = process.env.PORT || 55256;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
-  secret: process.env.DB_SESSION_SECRET,
-  cookie: {
-
-    maxAge: 0.5 * 60 * 60 * 1000
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: process.env.DB_SESSION_SECRET,
+    cookie: {
+        maxAge: 300000,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
 };
+
 app.use(session(sess));
 
-app.use(express.static('public'));
-
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use("/", allRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
 });
