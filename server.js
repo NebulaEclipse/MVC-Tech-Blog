7a391ae3466d2824d64f735187d6ed5b7b59c5ec
NebulaEclipse <chrisdouglas200516@gmail.com>
2024-03-27 16:41:39 -0400

    Attempting Heroku Fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 55256;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
